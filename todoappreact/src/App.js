d7cce1ddccaf9106eed4400f99f577b6b387d3d1
Asif Rana <muhammadasif@Muhammads-MacBook-Pro.local>
2020-11-24 17:55:20 +0000

    first commit

---
+++ todoappreact/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+    <TodoList />
+    <input type="text" />
+    </>
+  );
+}
+
+export default App;
---
+++ todoappreact/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TodoList(){
+    return (
+        <div>
+            <h3>this is heading</h3>
+            <h3>this is heading</h3>
+            <h3>this is heading</h3>
+        </div>
+    )
+}
